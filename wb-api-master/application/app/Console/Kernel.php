1f2b5ce7ef057ad020f78611d75671de1f6bf07a
pupik2 <lesha.parkhomenko.04@mail.ru>
2025-11-20 14:41:33 +0300

    Добавил новую фичу и исправил баги

--- wb-api-master/application/app/Console/Kernel.php
+++ wb-api-master/application/app/Console/Kernel.php
@@ -4,13 +4,13 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Illuminate\Support\Facades\Artisan;
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
@@ -21,16 +21,35 @@ class Kernel extends ConsoleKernel
             ->timezone(config('app.timezone'))
             ->withoutOverlapping()
             ->runInBackground();
+
+        $schedule->command('api:fetch sales')
+            ->cron('5 10,22 * * *')
+            ->withoutOverlapping()->runInBackground();
+
+        $schedule->command('api:fetch orders')
+            ->cron('7 10,22 * * *')
+            ->withoutOverlapping()->runInBackground();
+
+        $schedule->command('api:fetch incomes')
+            ->cron('9 10,22 * * *')
+            ->withoutOverlapping()->runInBackground();
+
+        $schedule->call(function () {
+            $d = now()->timezone(config('app.timezone'))->toDateString();
+
+            Artisan::call('api:fetch stocks',['--from'=>$d,'--to'=>$d]);
+
+        })
+            ->cron('11 10,22 * * *')
+            ->name('api:fetch:stocks:twice-daily')
+            ->withoutOverlapping()->runInBackground();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
+
+
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
